9f998fc660eef55316145f24effe4317d666aed9
mjuarez <mijuarez@microsoft.com>
2015-07-06 21:15:47 -0700

    Initial commit with 'hello world' data binding

---
+++ app/src/main/java/com/murki/alarmr/MainActivity.java
@@ -0,0 +1,43 @@
+package com.murki.alarmr;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.murki.alarmr.databinding.ActivityMainBinding;
+import com.murki.alarmr.viewmodels.MainActivityVM;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        MainActivityVM vm = new MainActivityVM("Hola Mundo.");
+        binding.setViewModel(vm);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/murki/alarmr/viewmodels/MainActivityVM.java
@@ -0,0 +1,14 @@
+package com.murki.alarmr.viewmodels;
+
+public class MainActivityVM {
+
+    private final String mainLine;
+
+    public MainActivityVM(String mainLine) {
+        this.mainLine = mainLine;
+    }
+
+    public String getMainLine() {
+        return mainLine;
+    }
+}
